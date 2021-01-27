14ba5ad605a9eb1b3209a9dd185a4abd42b18a26
Robin <robinholzwarth95@gmail.com>
2021-01-27 21:11:35 +0100

    Bugfix

--- rate_limiting/method_limiter.py
+++ rate_limiting/method_limiter.py
@@ -24,7 +24,7 @@ class MethodLimiter:
         method = "-".join(request.path.split("/")[1:5])
         print(method)
         try:
-            for limit in self.limits[method]:
+            for limit in self.limits[method].values():
                 limit.add
         except LimitBlocked as err:
             raise HTTPTooManyRequestsLocal(headers={"Retry-After": str(err.retry_after)})
