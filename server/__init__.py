7e96093f04fe3bb15b31ca2115ea180f777ff981
Robin <robinholzwarth95@gmail.com>
2021-01-28 23:27:01 +0100

    Added league_exp route

--- server/__init__.py
+++ server/__init__.py
@@ -27,6 +27,7 @@ class Proxy:
     from .match import match_v4_matches, match_v4_matchlists, match_v4_timelines
     from .summoner import summoner_v4_summoners
     from .league import league_v4_entries
+    from .league_exp import league_exp_v4_entries
     from .spectator import spectator_v4_active_games
 
     def __init__(self):
@@ -44,6 +45,7 @@ class Proxy:
             web.get('/lol/match/v4/timelines/{tail:.*}', self.match_v4_timelines),
             web.get('/lol/summoner/v4/summoners/{tail:.*}', self.summoner_v4_summoners),
             web.get('/lol/league/v4/entries/{tail:.*}', self.league_v4_entries),
+            web.get('/lol/league-exp/v4/entries/{tail:.*}', self.league_exp_v4_entries),
             web.get('/lol/spectator/v4/active-games/{tail:.*}', self.spectator_v4_active_games),
         ])
         return self.app
